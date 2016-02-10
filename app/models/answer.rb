c87b7e69cdefc01939b1c50780a5efe39c19dde9
yokozawa <y.yokozawa@gmail.com>
2016-02-10 09:32:50 +0900

    fix validate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,8 @@ class Answer < ActiveRecord::Base
   belongs_to :member
   belongs_to :question
 
-  validates :score, presence: true
-  validates :body, presence: true
+  # validates :score, presence: true
+  # validates :body, presence: true
 
   scope :mine, -> (member) {
     where(member_id: member.id)
